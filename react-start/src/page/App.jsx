eac1efb2774a7545ffbd9aa4f098b40f6b79a4d4
pwcong <pwcong@foxmail.com>
2018-12-21 14:18:38 +0800

    [*] update react-start

--- react-start/src/page/App.jsx
+++ react-start/src/page/App.jsx
@@ -5,23 +5,16 @@ import style from './style/app.scss';
 import logo from '../assets/imgs/logo.png';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-
-        return (
-            <div className={style.container}>
-
-                <img src={logo} className={style.logo} />
-                <p className={style.welcome}>React Start</p>
-
-            </div>
-        );
-
-    }
-
-
+  render() {
+    return (
+      <div className={style.container}>
+        <img src={logo} className={style.logo} />
+        <p className={style.welcome}>React Start</p>
+      </div>
+    );
+  }
 }
