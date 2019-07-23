7f6cb560b41e09ca6d04679be22ae366655277f7
彭伟聪 <pengwc@landray.com.cn>
2019-07-23 15:03:29 +0800

    update: dev命令调整

--- react-components/scripts/start/index.js
+++ react-components/scripts/start/index.js
@@ -1,15 +1,15 @@
+const path = require('path');
+const program = require('commander');
+const fs = require('fs-extra');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
 const VirtualModulesPlugin = require('webpack-virtual-modules');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-const path = require('path');
-const fs = require('fs-extra');
-const inquirer = require('inquirer');
-
-const { getPackages } = require('../utils');
+const { getPackagesMap } = require('../utils');
 const start = require('../utils/start');
-const clean = require('../clean');
-
-const packages = getPackages();
+const clean = require('../utils/clean');
 
 const getEntryCode = packagePath => {
   return `
@@ -21,54 +21,73 @@ ReactDOM.render(<App />,document.getElementById('app'))
   `;
 };
 
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'package',
-      message: '请选择开发测试的组件',
-      choices: packages.map(package => {
-        const json = fs.readJSONSync(path.join(package.path, 'package.json'));
-        return {
-          name: `${json.name} (${json.description || '-'})`,
-          value: package.path
-        };
-      })
-    }
-  ])
-  .then(answers => {
-    if (!answers.package) {
-      return;
-    }
+const startDev = packagePath => {
+  const distPath = path.join(packagePath, 'dist');
+  const testPath = path.join(packagePath, '__tests__');
 
-    clean();
-
-    const packagePath = answers.package;
-    const distPath = path.join(packagePath, 'dist');
-    const testPath = path.join(packagePath, '__tests__');
-
-    const entryPath = path.join(testPath, 'entry.tsx');
-    const virtualModules = new VirtualModulesPlugin({
-      [entryPath]: getEntryCode(
-        path.join(testPath, 'index.tsx').replace(/\\/g, '/')
-      )
-    });
-
-    const config = {};
-    const webpackConfig = {
-      entry: {
-        index: entryPath
-      },
-      output: {
-        path: distPath
-      },
-      plugins: [
-        virtualModules,
-        new HTMLWebpackPlugin({
-          template: path.join(__dirname, 'index.ejs')
-        })
-      ]
-    };
-
-    start(config, webpackConfig);
+  const entryPath = path.join(testPath, 'entry.tsx');
+  const virtualModules = new VirtualModulesPlugin({
+    [entryPath]: getEntryCode(
+      path.join(testPath, 'index.tsx').replace(/\\/g, '/')
+    )
   });
+
+  const config = {};
+  const webpackConfig = {
+    entry: {
+      index: entryPath
+    },
+    output: {
+      path: distPath
+    },
+    plugins: [
+      virtualModules,
+      new HTMLWebpackPlugin({
+        template: path.join(__dirname, 'index.ejs')
+      })
+    ]
+  };
+
+  start(config, webpackConfig);
+};
+
+async function main() {
+  clean();
+
+  const packagesMap = getPackagesMap();
+
+  program.parse(process.argv);
+  const args = program.args;
+  if (args.length === 1) {
+    const currentPackage = packagesMap[args[0]];
+    if (!currentPackage) {
+      console.log(chalk.red(`component ${args[0]} not found`));
+    }
+    startDev(currentPackage.path);
+  } else {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'package',
+          message: 'Please select the component for development testing',
+          choices: Object.keys(packagesMap).map(key => {
+            const package = packagesMap[key];
+            return {
+              name: `${package.name} (${package.description || '-'})`,
+              value: package.path
+            };
+          })
+        }
+      ])
+      .then(answers => {
+        if (!answers.package) {
+          return;
+        }
+
+        startDev(answers.package);
+      });
+  }
+}
+
+main();
